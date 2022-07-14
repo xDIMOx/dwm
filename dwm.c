fa9df6c7b9817454d7a1b8041789c0bd28c7f520
NRK <nrk@disroot.org>
2022-07-14 07:26:40 +0600

    do not call signal-unsafe function inside sighanlder
    die() calls vprintf, fputc and exit; none of these are
    async-signal-safe, see `man 7 signal-safety`.

--- dwm.c
+++ dwm.c
@@ -1661,6 +1661,8 @@ setup(void)
 	Atom utf8string;
 
 	/* clean up any zombies immediately */
+	if (signal(SIGCHLD, sigchld) == SIG_ERR)
+		die("can't install SIGCHLD handler:");
 	sigchld(0);
 
 	signal(SIGHUP, sighup);
@@ -1761,8 +1763,6 @@ showhide(Client *c)
 void
 sigchld(int unused)
 {
-	if (signal(SIGCHLD, sigchld) == SIG_ERR)
-		die("can't install SIGCHLD handler:");
 	while (0 < waitpid(-1, NULL, WNOHANG));
 }
 
